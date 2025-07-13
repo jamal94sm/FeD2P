3412216b7ab87542e127a1e7ce26751a51a5c401
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-07-13 09:20:09 +0330

    Update fedk2p.sh

--- fedk2p.sh
+++ fedk2p.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4 # 8, 16
 #SBATCH --mem=16G               # memory per node (ex: 16G) you can get more 
-#SBATCH --time=06:00:00 		   # time period you need for your code (it is 12 hours for example)
+#SBATCH --time=01:00:00 		   # time period you need for your code (it is 12 hours for example)
 #SBATCH --mail-user=<jamal73sm@gmail.com> 	# replace with your email address to get emails to know when it is started or failed. 
 #SBATCH --mail-type=ALL
 
