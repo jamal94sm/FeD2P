25a21f7bc65cdf998ab9f3d7f7b3b558bc861e01
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-07-16 23:01:32 +0330

    Update Config.py

--- Config.py
+++ Config.py
@@ -13,7 +13,7 @@ def get_args():
     parser.add_argument('--device', type=str, default="cuda" if torch.cuda.is_available() else "cpu") 
     #parser.add_argument('--device',default="mps" if torch.backends.mps.is_available() else "cpu")# for runing on mps MAC OS
     parser.add_argument('--num_clients', type=int, default=10)
-    parser.add_argument('--local_model_name', type=str, default="EfficientNet") # EfficientNet, ResNet20, ResNet18, ResNet10, MobileNetV2
+    parser.add_argument('--local_model_name', type=str, default="ResNet20") # EfficientNet, ResNet20, ResNet18, ResNet10, MobileNetV2
     parser.add_argument('--num_train_samples', type=int, default=50_000)
     parser.add_argument('--num_test_samples', type=int, default=3000)
     parser.add_argument('--dataset', type=str, default="cifar10")
