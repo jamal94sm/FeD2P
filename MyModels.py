877c54ecb7735f377f06848f6ec71dc45588331f
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-07-24 17:05:59 +0330

    Update MyModels.py

--- MyModels.py
+++ MyModels.py
@@ -17,9 +17,13 @@ def load_clip_model():
     model = transformers.CLIPModel.from_pretrained(model_name)
     processor = transformers.CLIPProcessor.from_pretrained(model_name, use_fast=False)
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_name, use_fast=False)
-
+    print(args.setup)
+    print(args.setup)
+    print(args.setup)
+    
     if "BN" in args.setup: 
         print("Unfreeze LayerNorm layers in the image encoder")
+        9/0
         # Unfreeze LayerNorm layers in the image encoder
         for module in model.vision_model.modules():
             if isinstance(module, torch.nn.LayerNorm):
