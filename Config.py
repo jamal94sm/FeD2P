d41d6d12ca1b3d4ff431712922af8f03e6e23dc9
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-11-05 01:32:39 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,10 +11,10 @@ def get_args():
     #parser.add_argument('--device',default="mps" if torch.backends.mps.is_available() else "cpu") # for runing on mps MAC OS
 
     parser.add_argument('--setup', default="local")
-    parser.add_argument('--output_name', type=str, default='_ResNet10_Fashion_10K_alpha0.1_')
+    parser.add_argument('--output_name', type=str, default='_ResNet18_CIFAR10_10K_alpha10_')
     
-    parser.add_argument('--local_model_name', type=str, default="ResNet10")
-    parser.add_argument('--dataset', type=str, default="fashion_mnist") # fashion_mnist, cifar10
+    parser.add_argument('--local_model_name', type=str, default="ResNet18")
+    parser.add_argument('--dataset', type=str, default="cifar10") # fashion_mnist, cifar10
 
     parser.add_argument('--num_clients', type=int, default= 10)
     parser.add_argument('--rounds', type=int, default=25)
@@ -23,7 +23,7 @@ def get_args():
     parser.add_argument('--num_test_samples', type=int, default=1000)
     parser.add_argument('--num_public_samples', type=int, default=1000)
     parser.add_argument('--num_synth_img_per_class', type=int, default=100)
-    parser.add_argument('--alpha_dirichlet', type=float, default=0.1)
+    parser.add_argument('--alpha_dirichlet', type=float, default=10)
     
     parser.add_argument('--num_prompts', type=int, default=10)
     parser.add_argument('--global_epochs', type=int, default=5)
