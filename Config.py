60049d558d35539172ace43e8dc1b7d411b2efde
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-11-05 01:17:13 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,7 +2,7 @@
 import argparse
 import torch
 
-'''
+
 # ===================== Argument Parsing =====================
 def get_args():
     parser = argparse.ArgumentParser(description="FedD2P")
@@ -11,18 +11,18 @@ def get_args():
     #parser.add_argument('--device',default="mps" if torch.backends.mps.is_available() else "cpu") # for runing on mps MAC OS
 
     parser.add_argument('--setup', default="local")
-    parser.add_argument('--output_name', type=str, default='_ResNet18_Fashion_2K_alpha10_')
+    parser.add_argument('--output_name', type=str, default='_ResNet10_Fashion_10K_alpha10_')
     
     parser.add_argument('--local_model_name', type=str, default="ResNet10")
     parser.add_argument('--dataset', type=str, default="fashion_mnist") # fashion_mnist, cifar10
 
     parser.add_argument('--num_clients', type=int, default= 10)
-    parser.add_argument('--rounds', type=int, default=30)
+    parser.add_argument('--rounds', type=int, default=25)
     
-    parser.add_argument('--num_train_samples', type=int, default=2_000)
-    parser.add_argument('--num_test_samples', type=int, default=200)
-    parser.add_argument('--num_public_samples', type=int, default=200)
-    parser.add_argument('--num_synth_img_per_class', type=int, default=20)
+    parser.add_argument('--num_train_samples', type=int, default=10_000)
+    parser.add_argument('--num_test_samples', type=int, default=1000)
+    parser.add_argument('--num_public_samples', type=int, default=1000)
+    parser.add_argument('--num_synth_img_per_class', type=int, default=100)
     parser.add_argument('--alpha_dirichlet', type=float, default=10)
     
     parser.add_argument('--num_prompts', type=int, default=10)
@@ -45,10 +45,10 @@ def get_args():
 
 args = get_args()
 
+
+
+
 '''
-
-
-
 # ===================== Toy Example =====================
 def get_args():
     parser = argparse.ArgumentParser(description="FedD2P")
@@ -88,4 +88,4 @@ def get_args():
     return parser.parse_args()
 
 args = get_args()
-
+'''
