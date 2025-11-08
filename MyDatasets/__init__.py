de851712340c4930a1723a877433399503241ec6
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-11-08 18:25:47 +0800

    Update __init__.py

--- MyDatasets/__init__.py
+++ MyDatasets/__init__.py
@@ -1,5 +1,6 @@
 from .cifar10 import load_dataset as load_cifar10
 from .eurosat import load_dataset as load_eurosat
 from .flowers import load_dataset as load_flowers
+from .imagenette import load_dataset as load_imagenette
 from .fashion_mnist import load_dataset as load_fashion_mnist
 from .utils import *
