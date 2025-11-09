49914d62e3f53154acf425c264c332ba7b61aab9
Jamal Seyedmohammadi <44075279+jamal94sm@users.noreply.github.com>
2025-11-09 10:29:19 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,16 +11,16 @@ def get_args():
     #parser.add_argument('--device',default="mps" if torch.backends.mps.is_available() else "cpu") # for runing on mps MAC OS
 
     parser.add_argument('--setup', default="local")
-    parser.add_argument('--output_name', type=str, default='_ResNet18_imagenette_7K_alpha10_')
-    parser.add_argument('--synth_path', type=str, default="/home/shahab33/projects/def-arashmoh/shahab33/GenFKD/Synthetic_Image/imagenette/")
+    parser.add_argument('--output_name', type=str, default='_ResNet18_cifar10_10K_alpha10_')
+    parser.add_argument('--synth_path', type=str, default="/home/shahab33/projects/def-arashmoh/shahab33/FedPD/Synthetic_Image/CIFAR10/")
     
     parser.add_argument('--local_model_name', type=str, default="ResNet18")
-    parser.add_argument('--dataset', type=str, default="imagenette") # animals, fashion_mnist, cifar10, flowers, eurosat
+    parser.add_argument('--dataset', type=str, default="cifar10") # cifar10, animals, fashion_mnist, cifar10, flowers, eurosat
 
-    parser.add_argument('--num_clients', type=int, default= 7)
+    parser.add_argument('--num_clients', type=int, default= 10)
     parser.add_argument('--rounds', type=int, default=30)
     
-    parser.add_argument('--num_train_samples', type=int, default=7_000)
+    parser.add_argument('--num_train_samples', type=int, default=10_000)
     parser.add_argument('--num_test_samples', type=int, default=1000)
     parser.add_argument('--num_public_samples', type=int, default=1000)
     parser.add_argument('--num_synth_img_per_class', type=int, default=100)
